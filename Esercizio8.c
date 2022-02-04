0d723aeae9c070e43107ff727f11da638cfd677d
Cesaro-Alessandro <94352342+Cesaro-Alessandro@users.noreply.github.com>
2022-02-04 10:08:14 +0100

    Add files via upload

---
+++ Esercizio8.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int nNumeri, numero, contatore = 0;
+    double somma = 0;
+    
+    do
+    {
+        printf("Inserisci numero totale di valori da inserire (deve essere positivo) \n");
+        scanf("%d", &nNumeri);
+    }
+    while(nNumeri < 0);
+
+    do
+    {
+        printf("Inserisci un valore > 0 \n");
+        scanf("%d", &numero);
+
+        if(numero > 0)
+        {
+            somma = sqrt(numero) + somma;
+            contatore++;
+        }
+    }
+    while(nNumeri != contatore && numero > 0);
+
+    if(numero < 0)
+    {
+        printf("Il programma è terminato perchè è impossibile effettuare la radice quadrata di un numero negativo \n");
+    }
+    printf("La somma totale è %f \n", somma);
+}
